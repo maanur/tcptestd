803d2e2bb547c024f81ad95730a7b4798cb630f8
Maanur <onmyouji777@gmail.com>
2017-06-17 01:18:51 +0300

    let heroku manage deps

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"net/http"
 
+	"sync"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		web()
+	}()
+	wg.Wait()
+}
+
+func web() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
@@ -16,3 +28,11 @@ func main() {
 	})
 	router.Run(":5000")
 }
+
+/*
+func listener() {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.GET
+	router.Run(":5001")
+}*/
